ad46cb7b363f698103357abaeb7ee226154f7c3d
200946357 <200946357@ingenieria.usac.edu.gt>
2015-06-09 23:28:26 -0600

    Version 2

--- test/practica1/metodosTest.java
+++ test/practica1/metodosTest.java
@@ -38,36 +38,20 @@ public class metodosTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of clasificacion method, of class metodos.
-     */
-    @Test
-    public void testClasificacion() {
-        System.out.println("clasificacion");
-        String tipo = "extranjero";
-        metodos instance = new metodos();
-        String expResult = "Estudiantes nacionales de universidades varias";
-        String result = instance.clasificacion(tipo);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        if (!result.equals(expResult))
-        {
-        fail("The test case is a prototype.");
-        }
-    }
 
     /**
      * Test of precionacional method, of class metodos.
      
+        */
     
     @Test
     public void testPrecionacional() {
         System.out.println("precionacional");
-        double precio = 250.0;
+        double precio = 250.0;  //Precio real
         metodos instance = new metodos();
-        double expResult = 342.5;
+        double expResult = 342.5;  // a Q250 el precio correcto es = 342.5
         double result = instance.precionacional(precio);
-        assertEquals(expResult, result, 0.0);
+        assertEquals(expResult, result, 0.0); //Comparacion de resultados para validar la prueba
         // TODO review the generated test code and remove the default call to fail.
         if (result != expResult)
         {
@@ -75,7 +59,6 @@ public class metodosTest {
         }
     }
     
-        */
     /**
      * Test of precioeextranjero method, of class metodos.
      */
@@ -84,7 +67,7 @@ public class metodosTest {
         System.out.println("precioeextranjero");
         double precio = 350.0;
         metodos instance = new metodos();
-        double expResult = 2215.0;
+        double expResult = 3101.0;   // A $350 el resultado es = 3101.0
         double result = instance.precioeextranjero(precio);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
